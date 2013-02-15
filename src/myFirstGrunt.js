6141460a559b4b7508f5f2bbc9f3ffedebcb5826
David Holloway <dholloway@criticalmass.com>
2013-02-15 13:40:50 +0000

    first commit

---
+++ src/myFirstGrunt.js
@@ -0,0 +1,28 @@
+/*
+ * myFirstGrunt
+ * https://github.com/dholloway/myFirstGrunt
+ *
+ * Copyright (c) 2013 David Holloway
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
